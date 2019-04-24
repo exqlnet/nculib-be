8dd620b4edc65b8d9a0b7a1ba5da587123f9801d
Exqlnet <exqlnet@gmail.com>
2019-04-24 16:13:47 +0800

    register restful resource

--- app/book/__init__.py
+++ app/book/__init__.py
@@ -2,3 +2,9 @@ from flask import Blueprint
 
 book_blueprint = Blueprint("book", __name__)
 
+from flask_restful import Api
+# from .resource.main import
+
+book_api = Api(book_blueprint)
+
+# book_api.add_resource()

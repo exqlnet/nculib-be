ba5b711a4856378248b22d85c7c2be9496ddc2a8
Exqlnet <exqlnet@gmail.com>
2019-04-29 16:44:17 +0800

    fix: book query api simplify book data

--- app/book/resource/query.py
+++ app/book/resource/query.py
@@ -23,7 +23,7 @@ class BookQuery(Resource):
             return {
                 "status": 0,
                 "message": "获取成功",
-                "data": [book.to_json() for book in pagination.items],
+                "data": [book.to_json_brief() for book in pagination.items],
                 "total_page": pagination.total,
                 "page": pagination.page
             }
@@ -32,7 +32,7 @@ class BookQuery(Resource):
             return {
                 "status": 0,
                 "message": "获取成功",
-                "data": [book.to_json() for book in books],
+                "data": [book.to_json_brief() for book in books],
             }
 
 

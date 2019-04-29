4fc611de388094eb863a46475e126e11ae6e84ea
Exqlnet <exqlnet@gmail.com>
2019-04-29 18:23:34 +0800

    fix bug: forget to add resource

--- app/book/__init__.py
+++ app/book/__init__.py
@@ -6,6 +6,7 @@ from flask_restful import Api
 from .resource.query import BookQuery, BookDetail
 from .resource.setting import SubjectSetting, UserSubjectSetting
 from .resource.recommend import Recommend
+from .resource.collect import UserCollect
 
 book_api = Api(book_blueprint)
 
@@ -15,3 +16,4 @@ book_api.add_resource(UserSubjectSetting, "/subject/info")
 book_api.add_resource(Recommend, "/recommend")
 
 book_api.add_resource(BookDetail, "/detail/<int:book_id>")
+book_api.add_resource(UserCollect, "/collect")

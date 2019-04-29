dafb81a49a780e5494875de0fca0e10bcb6f2f23
Exqlnet <exqlnet@gmail.com>
2019-04-29 18:12:30 +0800

    feature: add user collection table

--- app/user/model/user.py
+++ app/user/model/user.py
@@ -1,6 +1,10 @@
 from app import db
 from datetime import datetime
 
+user_collect = db.Table("user_collect",
+                        db.Column("user_id", db.Integer, db.ForeignKey("user.user_id"), primary_key=True),
+                        db.Column("book_id", db.Integer, db.ForeignKey("books.book_id"), primary_key=True))
+
 
 class User(db.Model):
     __tablename__ = "user"
@@ -9,3 +13,5 @@ class User(db.Model):
     open_id = db.Column(db.String(256), unique=True)
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     preference = db.Column(db.String(256), default="[]")
+
+    collect_books = db.relationship("Book", secondary=user_collect)
